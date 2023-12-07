75182cef6560697e80caa6c151e46db3a142b421
Kyle Miller <kmill31415@gmail.com>
2023-12-07 15:56:25 -0800

    day 7 part 2

--- AoC.lean
+++ AoC.lean
@@ -12,6 +12,7 @@ import AoC2023.Day5.Part2
 import AoC2023.Day6.Part1
 import AoC2023.Day6.Part2
 import AoC2023.Day7.Part1
+import AoC2023.Day7.Part2
 
 /-!
 Rather than modifying the lakefile (which causes a rebuild of std),
